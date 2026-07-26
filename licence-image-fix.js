e58dcb03d552795d9e8e01239626823a10d4d85b
abnieh-sazan-paydar <saeedkiadarbandsariii@gmail.com>
2026-07-26 21:19:17 +0330

    Point licence card to restored image only

--- licence-image-fix.js
+++ licence-image-fix.js
@@ -1,23 +1,9 @@
 (() => {
   const image = document.querySelector('#documents .document-image');
   if (!image) return;
-
+  const source = 'assets/images/documents/parvane-nezam-mohandesi-restored.jpg?v=20260726-3';
+  image.src = source;
+  image.removeAttribute('srcset');
   const link = image.closest('a');
-  const parts = [
-    'assets/images/documents/licence-part-00.txt?v=20260726-2',
-    'assets/images/documents/licence-part-01.txt?v=20260726-2',
-    'assets/images/documents/licence-part-02.txt?v=20260726-2'
-  ];
-
-  Promise.all(parts.map(path => fetch(path, { cache: 'no-store' }).then(response => {
-    if (!response.ok) throw new Error(path);
-    return response.text();
-  }))).then(chunks => {
-    const source = `data:image/webp;base64,${chunks.join('').replace(/\s+/g, '')}`;
-    image.src = source;
-    image.removeAttribute('srcset');
-    if (link) link.href = source;
-  }).catch(() => {
-    image.src = 'assets/images/documents/parvane-nezam-mohandesi.png';
-  });
+  if (link) link.href = source;
 })();
