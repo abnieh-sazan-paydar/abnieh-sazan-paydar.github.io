ce1e963cfa68d7cb78fb0cc5f5ddb37ac2a1cc81
abnieh-sazan-paydar <saeedkiadarbandsariii@gmail.com>
2026-07-26 21:19:31 +0330

    Force-load final profile and licence fixes only

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
   load('app-base.js?v=20260726-5', () => {
     load('service-flow.js?v=20260726-5', () => {
       load('footer-registration.js?v=20260726-1', () => {
-        load('about-slider.js?v=20260726-2', () => {
-          load('licence-image-fix.js?v=20260726-1');
+        load('about-slider.js?v=20260726-3', () => {
+          load('licence-image-fix.js?v=20260726-2');
         });
       });
     });
